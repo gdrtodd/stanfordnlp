2232b5a54725c5779e60159cdd19e6ad9f705a96
Graham Todd <gdrtodd@gmail.com>
2019-07-10 10:37:41 -0700

    Print statement in depparse trainer

--- stanfordnlp/models/depparse/trainer.py
+++ stanfordnlp/models/depparse/trainer.py
@@ -69,7 +69,10 @@ class Trainer(BaseTrainer):
 
         self.model.eval()
         batch_size = word.size(0)
-        _, preds = self.model(word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats, pretrained, lemma, head, deprel, word_orig_idx, sentlens, wordlens)
+        loss, preds = self.model(word, word_mask, wordchars, wordchars_mask, upos, xpos, ufeats, pretrained, lemma, head, deprel, word_orig_idx, sentlens, wordlens)
+        
+        print("IN TRAINER.PREDICT, LOSS = {}".format(loss.item()))
+
         head_seqs = [chuliu_edmonds_one_root(adj[:l, :l])[1:] for adj, l in zip(preds[0], sentlens)] # remove attachment for the root
         deprel_seqs = [self.vocab['deprel'].unmap([preds[1][i][j+1][h] for j, h in enumerate(hs)]) for i, hs in enumerate(head_seqs)]
 
